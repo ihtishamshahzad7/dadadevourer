16c9abfa69959a459c5f7a25a45f9393f2bfb77d
Ehtisham shahzad <147244422+ihtishamshahzad7@users.noreply.github.com>
2026-09-11 15:54:10 +0500

    feat(desktop): initialize updater plugin

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -4,6 +4,7 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running DadaDevourer");
 }
